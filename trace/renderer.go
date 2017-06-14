89aeda6e27ace02c802c45a8b88b75dc1f515aa2
Hunter Loftis <hunter@hunterloftis.com>
2017-06-13 23:37:50 -0700

    setting a pixel and writing a png

--- trace/renderer.go
+++ trace/renderer.go
@@ -1,11 +1,16 @@
 package trace
 
-import "os"
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
 
 // Renderer renders the results of a trace to a file
 type Renderer struct {
-	Width  uint
-	Height uint
+	Width  int
+	Height int
 }
 
 func (r *Renderer) Write(file string) (n int, err error) {
@@ -14,6 +19,7 @@ func (r *Renderer) Write(file string) (n int, err error) {
 		return 0, err
 	}
 	defer f.Close()
-	n, err = f.WriteString("Hello\n")
-	return n, err
+	m := image.NewRGBA(image.Rect(0, 0, r.Width, r.Height))
+	m.Set(5, 5, color.RGBA{255, 0, 0, 255})
+	return 0, png.Encode(f, m)
 }
