fedfc56d7058b0066b40ba15496dac012c7d198f
Hunter Loftis <hunter@hunterloftis.com>
2017-06-14 11:35:37 -0700

    Vector3 using value receivers

--- trace/camera.go
+++ trace/camera.go
@@ -17,8 +17,6 @@ func (c *Camera) Ray(x, y int) Ray3 {
 	px := (rx/float64(c.Width) - 0.5) * aspect
 	py := ry/float64(c.Height) - 0.5
 	projected := Vector3{px, py, -1}
-	// dir := projected.Minus(c.origin).Normalize() // why does Go complain with this?
-	diff := projected.Minus(c.Origin)
-	dir := diff.Normalize()
+	dir := projected.Minus(c.Origin).Normalize()
 	return Ray3{Origin: c.Origin, Dir: dir}
 }
--- trace/vector3.go
+++ trace/vector3.go
@@ -8,29 +8,28 @@ type Vector3 struct {
 }
 
 // Scale multiplies by a scalar
-// TODO: better to return a *Vector3 or Vector3?
-func (a *Vector3) Scale(n float64) Vector3 {
+func (a Vector3) Scale(n float64) Vector3 {
 	return Vector3{a.X * n, a.Y * n, a.Z * n}
 }
 
 // Dot returns the dot product of two vectors
 // (which is also the cosine of the angle between them)
-func (a *Vector3) Dot(b Vector3) float64 {
+func (a Vector3) Dot(b Vector3) float64 {
 	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
 }
 
 // Minus subtracts another vector from this one
-func (a *Vector3) Minus(b Vector3) Vector3 {
+func (a Vector3) Minus(b Vector3) Vector3 {
 	return Vector3{a.X - b.X, a.Y - b.Y, a.Z - b.Z}
 }
 
 // Normalize makes the vector of length 1
-func (a *Vector3) Normalize() Vector3 {
+func (a Vector3) Normalize() Vector3 {
 	d := a.Length()
 	return Vector3{a.X / d, a.Y / d, a.Z / d}
 }
 
 // Length finds the length of the vector
-func (a *Vector3) Length() float64 {
+func (a Vector3) Length() float64 {
 	return math.Sqrt(a.X*a.X + a.Y*a.Y + a.Z*a.Z)
 }
