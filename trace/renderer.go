e6be18e51de0e63f34079ca02e7861af7fe49e75
Hunter Loftis <hunter@hunterloftis.com>
2017-06-13 23:40:49 -0700

    simpler

--- trace/renderer.go
+++ trace/renderer.go
@@ -13,13 +13,13 @@ type Renderer struct {
 	Height int
 }
 
-func (r *Renderer) Write(file string) (n int, err error) {
+func (r *Renderer) Write(file string) error {
 	f, err := os.Create(file)
 	if err != nil {
-		return 0, err
+		return err
 	}
 	defer f.Close()
 	m := image.NewRGBA(image.Rect(0, 0, r.Width, r.Height))
 	m.Set(5, 5, color.RGBA{255, 0, 0, 255})
-	return 0, png.Encode(f, m)
+	return png.Encode(f, m)
 }
