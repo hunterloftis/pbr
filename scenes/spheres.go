7b9020ef589173fd904c85369dfb4f48842b501a
Hunter Loftis <hunter@hunterloftis.com>
2017-06-18 06:59:13 -0700

    nice composition
    Former-commit-id: 7e70af9674fcab098af21155a21d10af31f1f8fa

--- scenes/spheres.go
+++ scenes/spheres.go
@@ -15,7 +15,7 @@ func main() {
 	flag.Parse()
 
 	scene := trace.Scene{}
-	camera := trace.NewCamera(960, 540, 0.135)
+	camera := trace.NewCamera(960, 540, 0.050)
 	sampler := trace.NewSampler(&camera, &scene, 10)
 	renderer := trace.NewRenderer(&camera)
 	light := trace.NewLight(1000, 1000, 1000)
@@ -25,20 +25,20 @@ func main() {
 	silver := trace.NewMetal(0.972, 0.960, 0.915, 1)
 	gold := trace.NewMetal(1.022, 0.782, 0.344, 0.8)
 	glass := trace.NewGlass(0, 0, 0, 0, 1)
-	// frostedGlass := trace.NewGlass(0, 1, 0, 0.05, 0.8)
+	frostedGlass := trace.NewGlass(0, 1, 0, 0.05, 0.8)
 
 	// scene.SetEnv("images/glacier.hdr", 100)
-	scene.Add(trace.Sphere{trace.Vector3{0, 0, -4}, 0.1, silver})
+	scene.Add(trace.Sphere{trace.Vector3{-0.02, 0, -3.4}, 0.1, silver})
 	scene.Add(trace.Sphere{trace.Vector3{0.13, 0, -3}, 0.1, redPlastic})
-	scene.Add(trace.Sphere{trace.Vector3{0.3, 0, -3.5}, 0.1, gold})
-	scene.Add(trace.Sphere{trace.Vector3{-0.35, 0, -5}, 0.1, glass})
-	scene.Add(trace.Sphere{trace.Vector3{-0.2, 0, -7}, 0.1, bluePlastic})
-	// scene.Add(trace.Sphere{trace.Vector3{-1, 0, -4.0}, 0.1, silver})
+	scene.Add(trace.Sphere{trace.Vector3{0.42, 0, -3.5}, 0.1, gold})
+	scene.Add(trace.Sphere{trace.Vector3{-0.26, 0, -3.9}, 0.1, frostedGlass})
+	scene.Add(trace.Sphere{trace.Vector3{-0.27, 0, -2.9}, 0.1, glass})
+	scene.Add(trace.Sphere{trace.Vector3{-0.8, 0, -4}, 0.1, bluePlastic})
 	scene.Add(trace.Sphere{trace.Vector3{15.0, 25.0, -10.0}, 15.0, light})
 	scene.Add(trace.Sphere{trace.Vector3{0, -10000.1, -4}, 10000, whitePlastic})
-	camera.Move(0, 0.2, 0)
-	camera.LookAt(0, 0, -5)
-	camera.Focus(0, 0, -4, 2.8)
+	camera.Move(0, 0.15, -1.5)
+	camera.LookAt(0, 0, -4)
+	camera.Focus(0.13, 0, -3, 4)
 
 	frameSamples := (*samples) * sampler.Width * sampler.Height
 	fmt.Printf("Collecting %v frames, taking %v samples/frame...\n", *frames, frameSamples)
