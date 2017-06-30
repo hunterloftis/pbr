551d6e0b3aac3ee95ace9dc41cc48f2885332c31
Hunter Loftis <hunter@hunterloftis.com>
2017-06-29 21:20:41 -0400

    cube and sphere sitting on surface
    Former-commit-id: abbd0669198011e7d8d6d1f59e9b1e043d6399ba

--- cmd/cubes.go
+++ cmd/cubes.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	out := flag.String("out", "trace.png", "Output png filename.")
 	frames := flag.Int("frames", 4, "Number of frames to combine.")
-	samples := flag.Int("samples", 4, "Average per pixel samples to take.")
+	samples := flag.Int("samples", 10, "Average per pixel samples to take.")
 	heat := flag.String("heat", "", "Heatmap png filename.")
 	flag.Parse()
 
@@ -37,7 +37,7 @@ func main() {
 	})
 	scene.Add(&trace.Sphere{trace.Vector3{0, 0, -2}, 0.5, redPlastic})
 	scene.Add(&trace.Sphere{trace.Vector3{15.0, 25.0, -10.0}, 15.0, light})
-	scene.Add(&trace.Sphere{trace.Vector3{0, -10000.1, -4}, 10000, whitePlastic})
+	scene.Add(&trace.Sphere{trace.Vector3{0, -10000.5, 0}, 10000, whitePlastic})
 	camera.Move(-4, 4, 4)
 	camera.LookAt(0, 0, 0)
 	camera.Focus(0, 0, 0, 4)
