272a648bbf2c614d30c701dff00037d8c23a26c3
Hunter Loftis <hunter@hunterloftis.com>
2017-07-08 15:14:12 -0400

    use material constructor

--- cmd/cubes.go
+++ cmd/cubes.go
@@ -14,7 +14,7 @@ func main() {
 
 	light := pbr.Light(1500, 1500, 1500)
 	redPlastic := pbr.Plastic(1, 0, 0, 1)
-	// whiteGrid := pbr.Plastic(1, 1, 1, 0.8)
+	whiteGrid := pbr.Plastic(1, 1, 1, 0.8)
 	// grayLambert := pbr.Lambert(0.2, 0.2, 0.2, 0.1)
 	// bluePlastic := pbr.Plastic(0, 0, 1, 1)
 	silver := pbr.Metal(0.95, 0.93, 0.88, 1)
@@ -29,7 +29,7 @@ func main() {
 		pbr.UnitCube(pbr.Ident().Trans(0, 0, -0.4).Rot(pbr.Vector3{0, 0.1 * math.Pi, 0}).Scale(0.1, 0.1, 0.1), gold),
 		pbr.UnitCube(pbr.Ident().Trans(-0.3, 0, 0.3).Rot(pbr.Vector3{0, -0.1 * math.Pi, 0}).Scale(0.1, 0.1, 0.1), greenGlass),
 		pbr.UnitCube(pbr.Ident().Trans(0.175, 0.05, 0.175).Rot(pbr.Vector3{0, 0.55 * math.Pi, 0}).Scale(0.01, 0.2, 0.1), greenGlass),
-		pbr.UnitCube(pbr.Ident().Trans(0, -0.55, 0).Scale(1000, 1, 1000), (&pbr.Material{}).Init()),
+		pbr.UnitCube(pbr.Ident().Trans(0, -0.55, 0).Scale(1000, 1, 1000), whiteGrid),
 		pbr.UnitSphere(pbr.Ident().Trans(-0.2, 0.001, -0.2).Scale(0.1, 0.1, 0.1), greenGlass),
 		pbr.UnitSphere(pbr.Ident().Trans(0.3, 0.05, 0).Scale(0.2, 0.2, 0.2), greenGlass),
 		pbr.UnitSphere(pbr.Ident().Trans(7, 30, 6).Scale(30, 30, 30), light),
