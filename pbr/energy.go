208e96944067194cb22972fe5329d7a53b2f4ccc
Hunter Loftis <hunter@hunterloftis.com>
2017-07-09 14:10:21 -0400

    destroy not working
    Former-commit-id: 52c38eb3d29b48a7db2471c93a443f6b5edd1209

--- pbr/energy.go
+++ pbr/energy.go
@@ -1,5 +1,7 @@
 package pbr
 
+import "math/rand"
+
 // Energy is a way to store RGB light energy
 type Energy Vector3
 
@@ -11,4 +13,13 @@ func (a Energy) Gained(b Energy, signal Energy) Energy {
 // Scaled scales energy
 func (a Energy) Scaled(n float64) Energy {
 	return Energy{a.X * n, a.Y * n, a.Z * n}
+}
+
+// Destroy randomly destroys energy
+func (a Energy) Destroy(rnd *rand.Rand) (Energy, bool) {
+	risk := Vector3(a).Max()
+	if rnd.Float64() >= risk {
+		return a, true
+	}
+	return Energy{a.X / risk, a.Y / risk, a.Z / risk}, false
 }
