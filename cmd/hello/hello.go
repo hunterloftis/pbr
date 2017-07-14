579476d60540edf7473daf95dbd8508fd343cc23
Hunter Loftis <hunter@hunterloftis.com>
2017-07-14 10:06:42 -0400

    sketch
    Former-commit-id: 68e0252691f213f0cecdd709d64893e76a4e2e05

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"runtime"
+	"fmt"
 
 	"github.com/hunterloftis/pbr/pbr"
 )
 
 func main() {
-	workers := runtime.NumCPU()
 	scene := pbr.EmptyScene()
 	camera := pbr.NewCamera(1280, 720)
-	renderer := pbr.CamRenderer(camera)
-	monitor := pbr.NewMonitor(10)
+	sampler := pbr.NewSampler(camera, scene)
+	renderer := pbr.NewRenderer(sampler)
 
 	scene.SetSky(pbr.Vector3{40, 50, 60}, pbr.Vector3{})
-	scene.Add(pbr.UnitSphere(pbr.Ident(), pbr.Plastic(1, 1, 1, 0.8)))
+	scene.Add(pbr.UnitSphere(pbr.Ident(), pbr.Plastic(1, 1, 1, 0.8))) // TODO: (mat *Material, transforms ...*Matrix)
 
-	for i := 0; i < workers; i++ {
-		monitor.AddSampler(pbr.NewSampler(camera, scene))
-	}
-	for i := 0; i < workers; i++ {
-		renderer.Merge(<-m.Results)
+	for sampler.PerPixel() < 16 {
+		sampler.SampleFrame()
+		fmt.Println("\r%v samples", sampler.Count())
 	}
 	pbr.WritePNG("hello.png", renderer.Rgb())
 }
