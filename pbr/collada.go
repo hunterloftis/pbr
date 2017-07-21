75665c76e7137b85b28ed257c086720a6256e8e2
Hunter Loftis <hunter@hunterloftis.com>
2017-07-21 07:49:46 -0400

    more collada schema
    Former-commit-id: d16642e32ef82333ed2caf582f16c6c6a755142d

--- pbr/collada.go
+++ pbr/collada.go
@@ -3,22 +3,33 @@ package pbr
 // http://planet5.cat-v.org/
 // https://github.com/GlenKelley/go-collada/blob/master/import.go
 // https://larry-price.com/blog/2015/12/04/xml-parsing-in-go
+// http://htmlpreview.github.io/?https://github.com/utensil/lol-model-format/blob/master/references/Collada_Tutorial_1.htm
 type Collada struct {
 	Version  string     `xml:"attr"`
 	Geometry []Geometry `xml:"library_geometries>geometry"`
 }
 
 type Geometry struct {
-	Mesh Mesh `xml:"mesh"`
-	test int
-}
-
-type Mesh struct {
-	Source []Source `xml:"source"`
+	Source    []Source    `xml:"mesh>source"`
+	Triangles []Triangles `xml:"mesh>triangles"`
+	Input     []Input     `xml:"mesh>vertices>input"`
 }
 
 type Source struct {
 	FloatArray FloatArray `xml:"float_array"`
+}
+
+type Triangles struct {
+	Count    int     `xml:"count,attr"`
+	Material string  `xml:"material,attr"`
+	Input    []Input `xml:"input"`
+	Data     string  `xml:"p"`
+}
+
+type Input struct {
+	Semantic string `xml:"semantic,attr"`
+	Source   string `xml:"source,attr"`
+	Offset   int    `xml:"offset,attr"`
 }
 
 type FloatArray struct {
