6e3fce79b9df6f82aa564717e7b9fbaa47460120
Hunter Loftis <hunter@hunterloftis.com>
2018-01-21 23:00:18 -0500

    dropping in the old cube intersection code fixed it. need to see what the difference is since box is also uing the new code.

--- examples/shapes.go
+++ examples/shapes.go
@@ -14,9 +14,9 @@ import (
 
 func main() {
 	light := material.Light(1500, 1500, 1500)
-	redPlastic := material.Plastic(1, 0, 0, 1)
+	redPlastic := material.Plastic(0.9, 0, 0, 1)
 	whitePlastic := material.Plastic(1, 1, 1, 0.8)
-	bluePlastic := material.Plastic(0, 0, 1, 1)
+	bluePlastic := material.Plastic(0, 0, 0.9, 1)
 	greenPlastic := material.Plastic(0, 0.9, 0, 1)
 	gold := material.Metal(1.022, 0.782, 0.344, 0.9, 1)
 	greenGlass := material.Glass(0.2, 1, 0.1, 0.95)
@@ -34,7 +34,7 @@ func main() {
 		surface.UnitCube(gold).Move(0, 0, -0.4).Rotate(0, 0.1*math.Pi, 0).Scale(0.1, 0.1, 0.1),
 		surface.UnitCube(greenGlass).Move(-0.3, 0, 0.3).Rotate(0, -0.1*math.Pi, 0).Scale(0.1, 0.1, 0.1),
 		surface.UnitCube(greenGlass).Move(0.175, 0.05, 0.18).Rotate(0, 0.55*math.Pi, 0).Scale(0.02, 0.2, 0.2),
-		surface.UnitCube(whitePlastic).Move(0, -0.55, 0).Scale(1000, 1, 1000),
+		surface.UnitCube(whitePlastic).Move(0, -0.55, 0).Scale(1000, 1, 1000).SetGrid(bluePlastic, 1.0/20.0),
 		surface.UnitSphere(greenGlass).Move(-0.2, 0.001, -0.2).Scale(0.1, 0.1, 0.1),
 		surface.UnitSphere(bluePlastic).Move(0.3, 0.05, 0).Scale(0.2, 0.2, 0.2),
 		surface.UnitSphere(light).Move(7, 30, 6).Scale(30, 30, 30),
@@ -44,7 +44,7 @@ func main() {
 
 	fmt.Println("rendering shapes.png (3 minutes)...")
 	render.Start()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Minute * 30)
 	render.Stop()
 	render.WritePngs("shapes.png", "shapes-heat.png", "shapes-noise.png", 1)
 }
