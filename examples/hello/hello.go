53e6f1d8f44cc4bccd78828663b264669013e309
Hunter Loftis <hunter@hunterloftis.com>
2018-05-22 17:27:08 -0400

    more natural lighting

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	floor := surface.UnitCube(material.Default).Move(0, -1, 0).Scale(100, 1, 100)
-	halogen := material.Light(4000, 4000, 4000)
+	halogen := material.Light(4781, 4518, 4200)
 	light := surface.UnitSphere(halogen).Move(-5, 10, -1).Scale(5, 5, 5)
 	box := surface.UnitSphere(material.Chrome)
 	scene := pbr.NewScene(floor, light, box)
